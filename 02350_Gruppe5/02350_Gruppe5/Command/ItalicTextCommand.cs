0fce886ec233582594386d805ed7d75e9b8daed6
Mathias-Larsen <Mathias.e.larsen@gmail.com>
2013-11-19 09:20:19 +0100

    glkgslkd

---
+++ 02350_Gruppe5/02350_Gruppe5/Command/ItalicTextCommand.cs
@@ -0,0 +1,37 @@
+﻿using _02350_Gruppe5.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02350_Gruppe5.Command
+{
+    class ItalicTextCommand : IUndoRedoCommand
+    {
+        private ObservableCollection<ClassBox> classBoxs;
+        private ObservableCollection<ClassBox> selectedClassBox;
+
+        public ItalicTextCommand(ObservableCollection<ClassBox> _classBoxs, ObservableCollection<ClassBox> _selectedClassBox)
+        {
+            classBoxs = _classBoxs;
+            selectedClassBox = _selectedClassBox;
+        }
+
+        public void Execute()
+        {
+            ClassBox classBox = selectedClassBox.ElementAt(0);
+
+            selectedClassBox.Clear();
+            classBoxs.Remove(classBox);
+
+
+        }
+
+        public void UnExecute()
+        {
+
+        }
+    }
+}
